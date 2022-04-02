abb3db4bd877520773deca16d464e6d24f6f69cb
Anas CHAUVEAU <anas38.gt@gmail.com>
2022-04-02 08:36:22 +0200

    API Terminé ! + restructuration de models, controllers et routes

---
+++ Api/controllers/ctrlGlobal.js
@@ -0,0 +1,74 @@
+const db = require('../models/modelGlobal') // Requetes 
+
+const Connexion = async (req, res) => {
+
+    // Prelevé dans l'URL
+    let table = req.params.table;
+    let pseudo = req.params.pseudo;
+    let mdp = req.params.mdp;
+    //
+    
+    await db.Authentification(table, pseudo, mdp)
+    .then((data) => {
+        let err = false;
+        console.log(data)
+        res.json(data)
+    }).catch((err) => {
+        console.log(err)
+        res.json(err)
+    })
+}
+
+// POUR : Référent et Proviseur //
+const afficher_classe = async (req, res) => {
+
+    await db.getClasse()
+    .then((data) => {
+        let err = false;
+        console.log(data)
+        res.json(data)
+    }).catch((err) => {
+        console.log(err)
+        res.json(err)
+    })
+}
+
+// POUR : Professeur et Proviseur //
+const afficher_details_classe = async (req, res) => {
+
+    let idClasse = req.params.id;
+
+    await db.getEleve_classe(idClasse)
+    .then((data) => {
+        let err = false;
+        console.log(data)
+        res.json(data)
+    }).catch((err) => {
+        console.log(err)
+        res.json(err)
+    })
+}
+
+// POUR : Eleve et Proviseur //
+const afficher_note_eleve = async (req, res) => {
+    
+    let idEleve = req.params.idEleve;
+
+    await db.getNotes_Eleve(idEleve, idEleve)
+    .then((data) => {
+        let err = false;
+        console.log(data)
+        res.json(data)
+    }).catch((err) => {
+        console.log(err)
+        res.json(err)
+    })
+}
+
+// Exportation //
+module.exports = {
+    Connexion,
+    afficher_classe,
+    afficher_details_classe,
+    afficher_note_eleve  
+}
